8cc46bd190da97710bd6b0b1b63edcb926ee8bb4
bumwoo <qjadn0914@naver.com>
2019-02-11 21:48:17 +0900

    remove not use test

--- controller/infomation_test.go
+++ controller/infomation_test.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/labstack/echo"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
@@ -11,22 +10,22 @@ import (
 	"testing"
 )
 
-func TestInfomationController_GetAdvertisement(t *testing.T) {
-	req := httptest.NewRequest(echo.GET, "/advertisement", nil)
-	req.Header.Set(echo.HeaderAuthorization, TOKEN)
-	rec := httptest.NewRecorder()
-	ctx := echoApp.NewContext(req, rec)
-	assert.NoError(t, handleWithFilter(InfomationController{}.GetAdvertisement, ctx))
-	assert.Equal(t, http.StatusOK, rec.Code)
-
-	var v struct {
-		Result  []models.Advertisement `json:"result"`
-		Success bool                   `json:"success"`
-	}
-
-	require.NoError(t, json.Unmarshal(rec.Body.Bytes(), &v))
-	require.Equal(t, true, v.Success)
-}
+//func TestInfomationController_GetAdvertisement(t *testing.T) {
+//	req := httptest.NewRequest(echo.GET, "/advertisement", nil)
+//	req.Header.Set(echo.HeaderAuthorization, TOKEN)
+//	rec := httptest.NewRecorder()
+//	ctx := echoApp.NewContext(req, rec)
+//	assert.NoError(t, handleWithFilter(InfomationController{}.GetAdvertisement, ctx))
+//	assert.Equal(t, http.StatusOK, rec.Code)
+//
+//	var v struct {
+//		Result  []models.Advertisement `json:"result"`
+//		Success bool                   `json:"success"`
+//	}
+//
+//	require.NoError(t, json.Unmarshal(rec.Body.Bytes(), &v))
+//	require.Equal(t, true, v.Success)
+//}
 
 func TestInfomationController_GetEvent(t *testing.T) {
 	req := httptest.NewRequest(echo.GET, "/event", nil)
